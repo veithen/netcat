d618bf409ac458d37a58953ebf3aed6ede7ef09d
themnemonic <themnemonic@9a840629-f0a8-4623-a665-e2af6adeba08>
2002-05-11 19:46:08 +0000

    added file core.c 	updated interface for network.c functions
    git-svn-id: svn://svn.code.sf.net/p/netcat/code/trunk@128 9a840629-f0a8-4623-a665-e2af6adeba08

--- src/proto.h
+++ src/proto.h
@@ -5,7 +5,7 @@
  * Author: Giovanni Giacobbi <johnny@themnemonic.org>
  * Copyright (C) 2002  Giovanni Giacobbi
  *
- * $Id: proto.h,v 1.15 2002-05-06 20:37:13 themnemonic Exp $
+ * $Id: proto.h,v 1.16 2002-05-11 19:46:08 themnemonic Exp $
  */
 
 /***************************************************************************
@@ -21,6 +21,13 @@
  *   GNU General Public License for more details.                          *
  *                                                                         *
  ***************************************************************************/
+
+/* core.c */
+int core_readwrite(int sock, int sock2);
+int core_udp_connect(struct in_addr *host, unsigned short port);
+int core_udp_listen(struct in_addr *local_host, unsigned short local_port);
+int core_tcp_connect(struct in_addr *host, unsigned short port, int timeout);
+int core_tcp_listen(struct in_addr *local_host, unsigned short local_port, int timeout);
 
 /* flagset.c */
 bool netcat_flag_init();
@@ -44,6 +51,10 @@ extern bool opt_listen, opt_tunnel, opt_numeric, opt_random, opt_hexdump,
 		opt_udpmode, opt_telnet, opt_zero;
 extern int opt_interval, opt_verbose, opt_wait;
 extern char *opt_outputfile;
+extern FILE *output_fd;
+extern netcat_host local_host, remote_host;
+extern netcat_port local_port, remote_port;
+extern unsigned long bytes_sent, bytes_recv;
 
 /* network.c */
 bool netcat_resolvehost(netcat_host *dst, char *name);
@@ -51,9 +62,10 @@ bool netcat_getport(netcat_port *dst, const char *port_string,
 		    unsigned short port_num);
 int netcat_inet_pton(const char *src, void *dst);
 const char *netcat_inet_ntop(const void *src);
-int netcat_socket_new();
-int netcat_socket_new_connect(const struct in_addr *addr, unsigned short port,
-		const struct in_addr *local_addr, unsigned short local_port);
+int netcat_socket_new(int domain, int type);
+int netcat_socket_new_connect(int domain, int type, const struct in_addr *addr,
+		unsigned short port, const struct in_addr *local_addr,
+		unsigned short local_port);
 int netcat_socket_new_listen(const struct in_addr *addr, unsigned short port);
 int netcat_socket_accept(int fd, int timeout);
 
